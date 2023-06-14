da6d1ef1356b37c350a502c6849360afb7f1fe52
Rimunanda Ma'arif Parinduri <arimrimunanda15@gmail.com>
2023-06-14 12:36:54 +0700

    innitial Commit

---
+++ app/src/main/java/com/d3if3105/deliciouseats/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.d3if3105.deliciouseats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
