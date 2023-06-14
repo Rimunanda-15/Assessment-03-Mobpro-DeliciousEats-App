8964c1a169230ec65499b4a97bcf2ee829645e24
Rimunanda Ma'arif Parinduri <arimrimunanda15@gmail.com>
2023-06-14 15:06:53 +0700

    Menambahkan Fragment dan navigation component

--- app/src/main/java/com/d3if3105/deliciouseats/MainActivity.kt
+++ app/src/main/java/com/d3if3105/deliciouseats/MainActivity.kt
@@ -2,10 +2,18 @@ package com.d3if3105.deliciouseats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController = Navigation.findNavController(this,R.id.host_fargment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
