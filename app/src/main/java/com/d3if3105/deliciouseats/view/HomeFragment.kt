76841da5a7cdd74989b20701db8aefc2d0b0c77f
Rimunanda Ma'arif Parinduri <arimrimunanda15@gmail.com>
2023-06-14 23:07:20 +0700

    mengambil data dari api bagian 1 dan menggunakan glide

--- app/src/main/java/com/d3if3105/deliciouseats/view/HomeFragment.kt
+++ app/src/main/java/com/d3if3105/deliciouseats/view/HomeFragment.kt
@@ -1,11 +1,19 @@
 package com.d3if3105.deliciouseats.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.d3if3105.deliciouseats.api.RetrofitInstance
 import com.d3if3105.deliciouseats.databinding.FragmentHomeBinding
+import com.d3if3105.deliciouseats.model.Meal
+import com.d3if3105.deliciouseats.model.MealList
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 
 class HomeFragment : Fragment() {
@@ -22,6 +30,23 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        RetrofitInstance.foodApi.getRandomMeal().enqueue(object : retrofit2.Callback<MealList>{
+            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+                if(response.body() != null){
+                    val randomMeal : Meal = response.body()!!.meals[0]
+                    Glide.with(this@HomeFragment)
+                        .load(randomMeal.strMealThumb)
+                        .into(binding.imgRandomMeal)
+                }else{
+                    return
+                }
+            }
+
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.d("HomeFragment", t.message.toString())
+            }
+        })
     }
 
 }
